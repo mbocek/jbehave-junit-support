d15ff8a05684a1a443bfb3ec948233679460e482
Michal Bocek <michal.bocek@embedit.cz>
2017-04-13 15:20:42 +0200

    repackaging

---
+++ src/test/java/org/jbehavesupport/runner/story/AbstractStories.java
@@ -0,0 +1,36 @@
+package org.jbehavesupport.runner.story;
+
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+import org.jbehavesupport.runner.JUnitRunner;
+import org.jbehavesupport.runner.JUnitRunnerConfiguration;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+/**
+ * @author Michal Bocek
+ * @since 16/03/2017
+ */
+@RunWith(JUnitRunner.class)
+public abstract class AbstractStories extends JUnitStories {
+
+    public AbstractStories() {
+        JUnitRunnerConfiguration.recommendedConfiguration(configuredEmbedder());
+    }
+
+    @Override
+    public Configuration configuration() {
+        return new MostUsefulConfiguration();
+    }
+
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new InstanceStepsFactory(configuration(), getStepClasses());
+    }
+
+    protected abstract List<?> getStepClasses();
+}
